4878e550cbcf5a8b26ec0453f2cfca8493dac2ff
aureane moraes <aureane.moraes@gmail.com>
2021-01-27 13:44:13 -0300

    import iniciado

---
+++ app/Imports/FormImport.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Form;
+use Maatwebsite\Excel\Concerns\ToModel;
+//use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+//USE Maatwebsite\Excel\Concerns\WithProgressBar;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+
+class CityImport implements ToModel, WithHeadingRow, WithBatchInserts, WithUpserts, WithChunkReading, ShouldQueue,WithCustomCsvSettings
+{
+    //use Importable;
+
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => ',',
+            'input_encoding' => 'iso-8859-1'
+        ];
+    }
+
+    public function model(array $row)
+    {
+        return new Form([
+            'name' => $row['nome'],
+            'age' => $row['idade'],
+            'prioritarygroup' => $row['grupo_prioritario'],
+            'vacinationplace' => $row['observacoes']
+        ]);
+
+
+    }
+
+
+
+    // BachInsert
+    public function batchSize(): int
+    {
+        return 1;
+    }
+
+    // WithUpsert
+    public function uniqueBy()
+    {
+        return '*';
+    }
+
+    // Chunk Reading
+    public function chunkSize(): int
+    {
+        return 500;
+    }
+
+
+}
